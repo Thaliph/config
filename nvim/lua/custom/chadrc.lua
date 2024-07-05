fe4d81122994e8fea18c86f622ac8d60519dcb7a
Thaliph <25038189+Thaliph@users.noreply.github.com>
2024-07-05 15:03:57 +0200

    Update chadrc.lua

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,4 +1,9 @@
+---@type ChadrcConfig
 local M = {}
+
 M.ui = { theme = 'catppuccin' }
-M.plugins = 'custom.plugins'
+
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+
 return M
